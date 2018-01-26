d77d4c8fa97020eb1c42167cce850dd2d80da770
Isaacol77 <35587073+Isaacol77@users.noreply.github.com>
2018-01-26 16:07:08 -0600

    Add files via upload

---
+++ src/assignments/main_assignment2.py
@@ -0,0 +1,20 @@
+from assignment2 import faculty_evaluation_result
+
+'''Write code to call the faculty_evaluation_result function with data of your choice'''
+print('Hello')
+
+nev = 2
+rar = 5
+som = 34
+oft = 66
+voft = 45
+alw = 98
+
+total = alw + voft + oft + som + rar + nev
+
+alw_ratio = alw / total
+voft_ratio = voft / total
+oft_ratio = oft / total
+som_ratio = som / total
+rar_ratio = rar / total
+nev_ratio = nev / total
