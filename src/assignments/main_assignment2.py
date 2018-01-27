9030e5fba86ec371b4faf55eb90f0cf1e0351cd2
Isaacol77 <35587073+Isaacol77@users.noreply.github.com>
2018-01-27 17:32:46 -0600

    Update main_assignment2.py

--- src/assignments/main_assignment2.py
+++ src/assignments/main_assignment2.py
@@ -2,3 +2,6 @@ from assignment2 import faculty_evaluation_result
 
 '''Write code to call the faculty_evaluation_result function with data of your choice'''
 print('Hello')
+
+faculty_evaluation_result(18, 4, 3, 4, 34, 198)
+
